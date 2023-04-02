0d4139e217337581011ec1fffd7d8fc2ae870440
elisetchan <chan.el@northeastern.edu>
2023-04-01 23:19:14 -0400

    cleaned up formatting and bar chart

--- main_.js
+++ main_.js
@@ -23,18 +23,18 @@ window.initMap = function() {
 			// Converts the date list to a dict
 			dates = data.dates.split(", ");
 			const date_counts = [(dates[0].replace('\'', '').replace('[', '').substr(0, 10)),
-			(dates[1].replace('\'', '').substr(0, 10)),
-			(dates[2].replace('\'', '').substr(0, 10)),
-			(dates[3].replace('\'', '').substr(0, 10)),
-			(dates[4].replace('\'', '').replace(']', '').substr(0, 10))];
+			    (dates[1].replace('\'', '').substr(0, 10)),
+			    (dates[2].replace('\'', '').substr(0, 10)),
+			    (dates[3].replace('\'', '').substr(0, 10)),
+			    (dates[4].replace('\'', '').replace(']', '').substr(0, 10))];
 
 			// Converts the star list to a dict
 			stars = data.stars.split(", ");
 			const star_counts = {"1 star": (parseInt(stars[0].substr(1))),
-			"2 star": (parseInt(stars[1])),
-			"3 star": (parseInt(stars[2])),
-			"4 star": (parseInt(stars[3])),
-			"5 star": (parseInt(stars[4].substr(0, stars[4].length - 1)))};
+			    "2 star": (parseInt(stars[1])),
+			    "3 star": (parseInt(stars[2])),
+			    "4 star": (parseInt(stars[3])),
+			    "5 star": (parseInt(stars[4].substr(0, stars[4].length - 1)))};
 			
 			load_bar(star_counts, date_counts)
 		});
@@ -54,11 +54,11 @@ window.initMap = function() {
 			position: center,
 			icon: {url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"},
 			title: "You are Here"
-		});
+	});
 
 }
 
-document.getElementById("marker_button").addEventListener('click', handle_button);
+    document.getElementById("marker_button").addEventListener('click', handle_button);
 
 };
 
@@ -75,10 +75,10 @@ function load_bar(star_counts, date_counts) {
 	// Creates a frame for the bar chart
 	const FRAME2 =
 	d3.select("#vis2")
-	.append("svg")
-	.attr("height", frame_height)
-	.attr("width", frame_width)
-	.attr("class", "frame");
+	    .append("svg")
+	    .attr("height", frame_height)
+	    .attr("width", frame_width)
+	    .attr("class", "frame");
 
 
 	// Fills the bar chart with data from the csv
@@ -96,64 +96,78 @@ function load_bar(star_counts, date_counts) {
     	// Generates a set of scales for the x and y axes
 		const y_scale =
 		d3.scaleLinear()
-		.domain([0, max_star + 1])
-		.range([(frame_height - margins.bottom), margins.top]);
+		    .domain([0, max_star + 1])
+		    .range([(frame_height - margins.bottom), margins.top]);
 
 		const x_scale =
 		d3.scaleBand()
-		.domain(Object.keys(review_color))
-		.range([0, (frame_width - margins.right), 0]);
+		    .domain(Object.keys(review_color))
+		    .range([0, (frame_width - margins.right), 0]);
 
 		FRAME2.append("g")
-		.attr("transform", "translate(" + margins.left + ")")
-		.call(
-			d3.axisLeft()
-			.scale(y_scale)
-			.ticks(5)
+		    .attr("transform", "translate(" + margins.left + ")")
+		    .call(
+			    d3.axisLeft()
+			        .scale(y_scale)
+			        .ticks(5)
 			)
-		.attr("font-size", "20px");
+		    .attr("font-size", "20px");
 
 		FRAME2.append("g")
-		.attr("transform", "translate(" + margins.left + "," + (frame_height - margins.bottom) + ")")
-		.call(
-			d3.axisBottom()
-			.scale(x_scale)
-			.ticks(10)
+		    .attr("transform", "translate(" + margins.left + "," + (frame_height - margins.bottom) + ")")
+		    .call(
+			    d3.axisBottom()
+			        .scale(x_scale)
+			        .ticks(10)
 			);
 
     	// Adds to the correct bar for each restaurant
 		Object.entries(star_counts).forEach(entry => {
 			const [key, value] = entry;
 			FRAME2.append("rect")
-			.attr("transform", "translate(" + margins.left +  ")")
-			.attr("x", (x_scale(key)))
-			.attr("y", y_scale(value))
-			.attr("height", ( frame_height - y_scale(value) - margins.bottom))
-			.attr("width", x_scale.bandwidth() - 5)
-			.attr("class", "bar")
-			.attr("id", key);
+			    .attr("transform", "translate(" + margins.left +  ")")
+			    .attr("x", (x_scale(key)))
+			    .attr("y", y_scale(value))
+			    .attr("height", ( frame_height - y_scale(value) - margins.bottom))
+			    .attr("width", x_scale.bandwidth() - 5)
+			    .attr("class", "bar")
+			    .attr("id", key);
 		});
 
 		FRAME2.selectAll(".bar")
-		.attr("fill", (d,i) => {return colors(i)})
-		.attr("id", (d,i) => {return i})
+		    .attr("fill", (d,i) => {return colors(i)})
+		    .attr("id", (d,i) => {return i})
 
 		FRAME2.append("text")
-		.attr("x", ((frame_width/2) - (margins.right)))
-		.attr("y", (margins.top))
-		.text("Review Distribution")
-		.attr("class", "title");
+		    .attr("x", ((frame_width) - (margins.right))/2)
+		    .attr("y", margins.top)
+		    .text("Review Distribution")
+		    .attr("class", "title");
+
+		FRAME2.append("text")
+		    .attr("x", ((frame_width) )/2)
+		    .attr("y", (frame_height - margins.top/2))
+		    .text("Yelp Star Rating")
+		    .attr("class", "label");
+
+		FRAME2.append("text")
+		    .attr("x", (-frame_height/2) - margins.top/2)
+		    .attr("y", ((margins.top/2)))
+		    .attr("transform", "rotate(270)")
+		    .text("Review Count")
+		    .attr("class", "label");
 
     	// Creates a tooltip for the bar chart
 		const tooltip =
 		d3.select("#vis2")
-		.append("div")
-		.attr("class", "tooltip")
-		.style("opacity", 0);
+		    .append("div")
+		    .attr("class", "tooltip")
+		    .style("opacity", 0);
 
     	// Shows the tooltip when the mouse moves over a bar
 		function handleMouseover(event, d) {
 			tooltip.style("opacity", 1);
+		    d3.select(this).style("opacity", 0.5);
 		}
 
     	// Updates the tooltip with the correct information
@@ -166,13 +180,15 @@ function load_bar(star_counts, date_counts) {
     	// Hides the tooltip when the mouse leaves a bar
 		function handleMouseleave(event, d) {
 			tooltip.style("opacity", 0);
+			FRAME2.selectAll(".bar")
+                .style("opacity", 1);
 		}
 
     	// Attaches event handlers to all the bars
 		FRAME2.selectAll(".bar")
-		.on("mouseover", handleMouseover)
-		.on("mousemove", function () {handleMousemove(0,0,this)})
-		.on("mouseleave", handleMouseleave);
+		    .on("mouseover", handleMouseover)
+		    .on("mousemove", function () {handleMousemove(0,0,this)})
+		    .on("mouseleave", handleMouseleave);
 	});
 }
 
